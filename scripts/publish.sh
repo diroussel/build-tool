fda1ed60e4bdd973a0303b5880033e35f3b5dd4f
Sean Marjason <sean.marjason@nhs.net>
2022-04-26 10:38:56 +0000

    AVWD-302: Amend repo to publish under namespace

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,8 @@ set -o pipefail
 
 CODEARTIFACT_DOMAIN="repo"
 CODEARTIFACT_DOMAIN_OWNER=$POPH_MGMT_ACCOUNT
-CODEARTIFACT_PACKAGE="riskstrat-lambda-package-tool"
+CODEARTIFACT_NAMESPACE="riskstrat"
+CODEARTIFACT_PACKAGE="lambda-package-tool"
 
 echo "============= Publishing to AWS CodeArtifact ${PRIVATE_CODE_REPOSITORY} repo =============="
 
@@ -20,40 +21,51 @@ if [[ "${PRIVATE_CODE_REPOSITORY}" == "private_staging" ]]; then
   # Check if package has been published.
   PUBLISHED_PACKAGES=$(aws codeartifact list-packages --domain ${CODEARTIFACT_DOMAIN} --repository ${PRIVATE_CODE_REPOSITORY} --domain-owner ${CODEARTIFACT_DOMAIN_OWNER} | jq '.packages')
 
-  # If package has been published.
-  if [[ "${PUBLISHED_PACKAGES[*]})" =~ "${CODEARTIFACT_PACKAGE}" ]]; then
+  for row in $(echo "${PUBLISHED_PACKAGES}" | jq -c '.[]'); do 
+    NAMESPACE=$(jq -r '.namespace' <<< $row)
+    PACKAGE=$(jq -r '.package' <<< $row)
 
-    # Get latest published version.
-    LATEST_PUBLISHED_VERSION=$(aws codeartifact list-package-versions \
-                                  --domain ${CODEARTIFACT_DOMAIN} \
-                                  --domain-owner ${CODEARTIFACT_DOMAIN_OWNER} \
-                                  --format npm \
-                                  --repository ${PRIVATE_CODE_REPOSITORY} \
-                                  --package ${CODEARTIFACT_PACKAGE} \
-                                  | jq -r '.defaultDisplayVersion')
 
-    # Get current package version.
-    PACKAGE_VERSION=$(jq -r '.version' package.json)
+    #   # If package has been published.
+    if [[ "@${NAMESPACE}/${PACKAGE}" == "@${CODEARTIFACT_NAMESPACE}/${CODEARTIFACT_PACKAGE}" ]]; then
 
-    # If package version currently exists, delete the package before publishing
-    if [[ "${LATEST_PUBLISHED_VERSION}" == "${PACKAGE_VERSION}" ]]; then
-      echo "Deleting previously published package from staging"
-      echo "Version to be deleted: $PACKAGE_VERSION"
+      echo "Package previously published. Checking version numbers."
 
-      aws codeartifact delete-package-versions \
-      --domain $CODEARTIFACT_DOMAIN \
-      --domain-owner $CODEARTIFACT_DOMAIN_OWNER \
-      --format npm \
-      --repository $PRIVATE_CODE_REPOSITORY \
-      --package $CODEARTIFACT_PACKAGE \
-      --versions $PACKAGE_VERSION
+      # Get latest published version.
+      LATEST_PUBLISHED_VERSION=$(aws codeartifact list-package-versions \
+                                    --domain ${CODEARTIFACT_DOMAIN} \
+                                    --domain-owner ${CODEARTIFACT_DOMAIN_OWNER} \
+                                    --format npm \
+                                    --repository ${PRIVATE_CODE_REPOSITORY} \
+                                    --namespace ${CODEARTIFACT_NAMESPACE} \
+                                    --package ${CODEARTIFACT_PACKAGE} \
+                                    | jq -r '.defaultDisplayVersion')
 
-    else
-      echo "Version does not yet exist. Proceeding to publish."
+      # Get current package version.
+      PACKAGE_VERSION=$(jq -r '.version' package.json)
+
+      # If package version currently exists, delete the package before publishing
+      if [[ "${LATEST_PUBLISHED_VERSION}" == "${PACKAGE_VERSION}" ]]; then
+        echo "Deleting previously published package from staging"
+        echo "Version to be deleted: $PACKAGE_VERSION"
+
+        aws codeartifact delete-package-versions \
+        --domain $CODEARTIFACT_DOMAIN \
+        --domain-owner $CODEARTIFACT_DOMAIN_OWNER \
+        --format npm \
+        --repository $PRIVATE_CODE_REPOSITORY \
+        --namespace ${CODEARTIFACT_NAMESPACE} \
+        --package $CODEARTIFACT_PACKAGE \
+        --versions $PACKAGE_VERSION
+
+      else
+        echo "Version does not yet exist. Proceeding to publish."
+      fi
+
     fi
-  else
-    echo "Package does not yet exist. Proceeding to publish."
-  fi
+
+  done
+
 fi
 
 echo "Publishing ..."
