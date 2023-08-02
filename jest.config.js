3c19f8b569f8237d123d92a86d52320c6006cf63
David Roussel <dave@dvae.net>
2023-08-02 21:37:16 +0100

    upgrade jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   rootDir: '.',
   testEnvironment: 'node',
-  verbose: true,
   moduleDirectories: ['node_modules'],
-  setupFilesAfterEnv: ['jest-allure/dist/setup', './setupAllure.js'],
   transformIgnorePatterns: ['node_modules'],
   transform: {
     '^.+\\.(js|ts)$': ['babel-jest'],
@@ -15,20 +13,5 @@ module.exports = {
     '!src/package-cli.ts',
   ],
   coveragePathIgnorePatterns: ['__test__', '__tests__'],
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        outputDirectory: `reports/test_reports`,
-        outputName: `jest-junit.xml`,
-      },
-    ],
-    'jest-allure',
-  ],
   maxWorkers: '50%',
-  // As of Jest 27, the default has changed from 'jasmine' to 'jest-circus/runner'. However, this project uses jest-allure,
-  // which is dependent on the jasmine test runner
-  testRunner: 'jest-jasmine2',
-  setupFiles: ['./jest-setup.js'],
 };
